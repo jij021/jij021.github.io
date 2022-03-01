2d274cd7e1b543184fd2f73fe74dec27cf8df38a
Janice <jij021@newschool.edu>
2022-03-01 10:11:37 -0500

    Added Javascript

--- studio-2022/worldbuilding/script.js
+++ studio-2022/worldbuilding/script.js
@@ -1,9 +1,34 @@
-let hiddenElement = document.getElementById("hidden");
-
 function show() {
-  hiddenElement.style.display = "block";
+  var hiddenElement = document.getElementById("hidden");
+
+  hiddenElement.classList.replace("hidden", "show");
+}
+
+function showFlex() {
+  var hiddenElement = document.getElementById("hidden");
+
+  hiddenElement.classList.replace("hidden", "show-flex");
+}
+
+function tickerChange() {
+  var ticker1 = document.getElementById("ticker-1");
+  var ticker2 = document.getElementById("ticker-2");
+
+  ticker1.style.display = "none";
+
+  ticker2.classList.replace("hidden", "ticker-text");
 }
 
 function pause() {
+  var tickerText = document.getElementById("ticker-text");
 
+  tickerText.classList.toggle("pause");
+}
+
+function pauseAlt() {
+  var tickerText1 = document.getElementById("ticker-1");
+  var tickerText2 = document.getElementById("ticker-2");
+
+  tickerText1.classList.toggle("pause");
+  tickerText2.classList.toggle("pause");
 }
