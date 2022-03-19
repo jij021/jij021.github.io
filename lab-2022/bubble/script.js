77ca7935ddd4c7003ced86b00de5583e237fd206
Janice <jij021@newschool.edu>
2022-03-18 23:21:28 -0400

    Bubble demo

---
+++ lab-2022/bubble/script.js
@@ -0,0 +1,15 @@
+let container = document.querySelector('#container');
+
+for (i = 0; i < 50; i++) {
+    let bubble = document.createElement('div');
+    bubble.classList.add('bubble');
+    bubble.style.left= (90 * Math.random()) + '%';
+    bubble.style.top= (90 * Math.random()) + '%';
+    container.appendChild(bubble);
+}
+
+container.addEventListener('click', function(event) {
+    if (event.target.classList.contains('bubble')) {
+        event.target.remove();
+    }
+})
