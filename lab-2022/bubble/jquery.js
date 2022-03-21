12cf210850952b9e9f6da88d7f210386098a0fc7
Janice <jij021@newschool.edu>
2022-03-20 21:57:36 -0400

    Stylized and added JQ

---
+++ lab-2022/bubble/jquery.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $("#rubber-duck").draggable({containment: "#container"});
+    $("#soap").draggable({containment: "#container"});
+    $("#bath-towel").draggable({containment: "#container"});
+})
--- lab-2022/bubble/script.js
+++ lab-2022/bubble/script.js
@@ -1,11 +1,13 @@
 let container = document.querySelector('#container');
 
-for (i = 0; i < 50; i++) {
-    let bubble = document.createElement('div');
-    bubble.classList.add('bubble');
-    bubble.style.left= (90 * Math.random()) + '%';
-    bubble.style.top= (90 * Math.random()) + '%';
-    container.appendChild(bubble);
+function bubbles() {
+    for (i = 0; i < 15; i++) {
+        let bubble = document.createElement('div');
+        bubble.classList.add('bubble');
+        bubble.style.left= (90 * Math.random()) + '%';
+        bubble.style.top= (90 * Math.random()) + '%';
+        container.appendChild(bubble);
+    }
 }
 
 container.addEventListener('click', function(event) {
