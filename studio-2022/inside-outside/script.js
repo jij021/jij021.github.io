c425bf6e1878b0ca0b9a703fe3316e63417c67a4
Janice <jij021@newschool.edu>
2022-04-07 01:15:37 -0400

    Images and layout

--- studio-2022/inside-outside/script.js
+++ studio-2022/inside-outside/script.js
@@ -1,5 +1,35 @@
 console.log("hello world");
 
+// other functions -------------------------------------------
+
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menu-item");
+const hamburger= document.querySelector(".hamburger");
+const closeIcon= document.querySelector("#close-icon");
+const menuIcon = document.querySelector("#menu-icon");
+
+function toggleMenu() {
+  if (menu.classList.contains("show-menu")) {
+    menu.classList.remove("show-menu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
+  } else {
+    menu.classList.add("show-menu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
+  }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+menuItems.forEach( 
+    function(menuItem) { 
+      menuItem.addEventListener("click", toggleMenu);
+    }
+)
+
+// airtable data -------------------------------------------
+
 var Airtable = require('airtable');
 Airtable.configure({
     endpointUrl: 'https://api.airtable.com',
@@ -47,11 +77,34 @@ function consoleLogImages() {
     })
 }
 
-function showImages() {
-    console.log("showImages()");
-    images.forEach((image) => {
-        const h2 = document.createElement("h2");
-        h2.innerText = image.fields.Title;
-        document.body.appendChild(h2);
-    })
-}
+// function showBook(img, div) {
+//     console.log("showBook()", img);
+  
+//     // find the img detail element
+//     const bookDetail = document.getElementById("book-detail");
+  
+//     // populate the template with the data in the provided book
+//     bookDetail.getElementsByClassName("title")[0].innerText = img.fields.title; 
+//     bookDetail.getElementsByClassName("author")[0].innerText = "By " + img.fields.author; 
+//     bookDetail.getElementsByClassName("description")[0].innerText =
+//     img.fields.description;
+//     bookDetail.getElementsByClassName("more")[0].href = img.fields.more;
+//     bookDetail.getElementsByClassName("cover-image")[0].src =
+//     img.fields.cover_image[0].url;
+  
+//     // randomly rotate the cover image a little
+//     document.querySelector(".cover-image").style.transform = getRandomRotation();
+  
+//     // remove the .active class from any book spines that have it...
+//     const shelf = document.getElementById("shelf");
+//     const bookSpines = shelf.getElementsByClassName("active");
+//     for (const bookSpine of bookSpines) {
+//       bookSpine.classList.remove("active");
+//     }
+//     // ...and set it on the one just clicked
+//     div.classList.add("active");
+  
+//     // reveal the detail element, we only really need this the first time
+//     // but its not hurting to do it more than once
+//     bookDetail.classList.remove("hidden");
+//   }
