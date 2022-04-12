8df85c6533768f4215196793cc4d46cf0dfbb18c
Janice <jij021@newschool.edu>
2022-04-12 00:42:56 -0400

    Gradients

--- studio-2022/inside-outside/script.js
+++ studio-2022/inside-outside/script.js
@@ -2,31 +2,7 @@ console.log("hello world");
 
 // other functions -------------------------------------------
 
-const menu = document.querySelector(".menu");
-const menuItems = document.querySelectorAll(".menu-item");
-const hamburger= document.querySelector(".hamburger");
-const closeIcon= document.querySelector("#close-icon");
-const menuIcon = document.querySelector("#menu-icon");
 
-function toggleMenu() {
-  if (menu.classList.contains("show-menu")) {
-    menu.classList.remove("show-menu");
-    closeIcon.style.display = "none";
-    menuIcon.style.display = "block";
-  } else {
-    menu.classList.add("show-menu");
-    closeIcon.style.display = "block";
-    menuIcon.style.display = "none";
-  }
-}
-
-hamburger.addEventListener("click", toggleMenu);
-
-menuItems.forEach( 
-    function(menuItem) { 
-      menuItem.addEventListener("click", toggleMenu);
-    }
-)
 
 // airtable data -------------------------------------------
 
