f14f0d2e2032efe49845ec5487e621a00b61a454
Janice <jij021@newschool.edu>
2022-04-14 18:07:26 -0400

    Randomized display of ads

--- studio-2022/inside-outside/script/global.js
+++ studio-2022/inside-outside/script/global.js
@@ -1,7 +1,27 @@
-document.querySelector("#about").addEventListener('click', () => {
-    let aboutSVG = document.querySelector("#about");
-    let aboutText = document.querySelector("#about-text");
-    aboutText.classList.toggle("display");
+let on = true;
+let aboutOutline = document.querySelector("#about-outline");
+let aboutFilled = document.querySelector("#about-filled");
+let aboutText = document.querySelector("#about-text");
 
-    if()
+function toggle() {
+	if (on) { fadeIn(); } else { fadeOut(); }
+	on = !on;
+}
+
+function fadeOut(){
+	aboutText.style.opacity = 0;
+    aboutOutline.style.display = "block";
+    aboutFilled.style.display = "none";
+}
+
+function fadeIn(){
+	aboutText.style.opacity = 1;
+    aboutOutline.style.display = "none";
+    aboutFilled.style.display = "block";
+}
+
+document.querySelector("#about").addEventListener('click', () => {
+    toggle();
+    document.querySelector("nav").classList.toggle("lower-opacity");
+    document.querySelector("main").classList.toggle("lower-opacity");
 });
--- studio-2022/inside-outside/script/script-garden.js
+++ studio-2022/inside-outside/script/script-garden.js
@@ -11,7 +11,7 @@ console.log(Airtable);
 base("Cool-Images").select({}).eachPage(gotPageOfImages, gotAllImages);
 
 // an empty array to hold our Images data
-const images = [];
+let images = [];
 
 // callback function that receives our data
 function gotPageOfImages(records, fetchNextPage) {
@@ -60,24 +60,43 @@ function showImages() {
     const img = document.createElement("img");
     img.classList.add("ad-image");
     img.src = image.fields.attachments[0].url;
+
     // put the img tags into the figure
     adSection.appendChild(img);
 
     if(image.fields.category == "banner"){
-        img.style.width = 30 + '%';
+        img.style.width = 32 + '%';
     }
+    if(image.fields.category == "reduce-size"){
+        img.style.width = 6 + '%';
+    }
+
+    randomSelection();
 
     // randomizes the image locations
     randomImageLocation(img);
 
     document.querySelector("#refresh").addEventListener('click', () => {
         randomImageLocation(img);
+        randomSelection();
     });
   });
+}
+
+// randomly select a section of the ads to display (between 3-20 ideally)
+function randomSelection() {
+    var imgAds = document.querySelectorAll(".ad-image");
+    var random = Math.floor(Math.random() * imgAds.length);
+    var randomAd = imgAds[random];
+    noDisplay(randomAd);
+}
+
+function noDisplay(randomAd) {
+    randomAd.classList.toggle("no-display");
 }
 
 // randomizes the image locations
 function randomImageLocation(img) {
     img.style.left= (90 * Math.random()) + '%';
-    img.style.bottom= (60 * Math.random()) + '%';
+    img.style.bottom= (80 * Math.random()) + '%';
 }
