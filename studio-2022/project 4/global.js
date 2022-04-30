c81cff388de0c7e2cc531969d7c96345971aee65
Janice <jij021@newschool.edu>
2022-04-30 19:30:33 -0400

    Bound keys to colors and added instructions

--- studio-2022/project 4/global.js
+++ studio-2022/project 4/global.js
@@ -1,17 +1,13 @@
 console.log("CONSOLE ready");
 
-function renameJournal() {
-    document.title = prompt("Name your journal:");
-}
-
-// what i want to happen:
-// 1. on all keypresses, a random color will appear on the right side
-// 2. detect certain words, which will trigger specific colors, sizes, or shapes
-//    ie, typing "happy" will make a soft, yellow circle appear on the canvas. however, typing "sad" will make a sharp purple triangle appear on the canvas
+// to do:
+// 1) make instruction toggle
+// 2) set a timer to delete divs after a certain period of time, fading out
 
 $(document).ready(function() {
     console.log("JQUERY ready");
 
+    // types the letters
     function createLetter(key) {
         var cursor = document.querySelector("#cursor");
         var span = document.createElement("span");
@@ -19,18 +15,63 @@ $(document).ready(function() {
         cursor.parentNode.insertBefore(span, cursor);
     }
 
+    // deletes letters on backspace press
+    // also deletes the last div in canvas container
     function deleteElement() {
         var letter = document.querySelector("#cursor").previousSibling;
         if (letter) {
           letter.remove();
         }
+        var canvasContainer = document.querySelector("#canvas-container");
+        canvasContainer.removeChild(canvasContainer.lastChild);
+    }
+
+    // allow for random HSL colors
+    function getRandomColor() {
+        return 'hsla(' + (Math.random() * 360) + ', 95%, 75%, 1)';
+    }
+
+    // allow for random sizes
+    function getRandomSize() {
+        let randomNum = Math.random();
+        let min = 0;
+        let max = 2;
+        return 'scale(' + (randomNum * (max - min) + min) + ',' + (randomNum * (max - min) + min) + ')';
+    }
+
+    // put in random position 
+    // this is for mobile sizes
+    function getRandomPos(div) {
+        div.style.left= (100 * Math.random()) + '%';
+        div.style.top= (100 * Math.random()) + '%';
+    }
+
+    // create new divs with color gradients at a random position and random color
+    function makeCircle() {
+        // check if function is working
+        console.log("makeCircle()");
+        const canvasContainer = document.querySelector("#canvas-container");
+        var div = document.createElement("div");
+
+        // add the div to the canvas container so it's visible
+        canvasContainer.appendChild(div);
+
+        // add the circle class to the div, showing the circle
+        div.classList.add("circle");
+
+        // randomize the position, color, and size of circle
+        getRandomPos(div);
+        div.style.backgroundColor = getRandomColor();
+        div.style.transform = getRandomSize();
     }
 
     $(document).on("keydown", function(e) {
         if ("abcdefghijklmnopqrstuvwxyz!,'.;:?0123456789".includes(e.key.toLowerCase())) {
             createLetter(e.key);
+            makeCircle();
         } else if (e.keyCode == 32){
             createLetter("&nbsp;");
+            makeCircle();
         }
         else if (e.keyCode == 8) {
             deleteElement();
