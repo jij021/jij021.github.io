b6a0a179f51d72830101b6fcc942c8260fffc31a
Janice Ji <jij021@newschool.edu>
2023-11-30 05:59:24 -0500

    minor edits

--- fall-2023/homepage/scripts/survey-result-more-info.js
+++ fall-2023/homepage/scripts/survey-result-more-info.js
@@ -66,54 +66,54 @@ if(myRoll2 == 3){
 }
 
 // showing the correct response in the scale ------------------------------------------------------------------------------------------------------
-if(behavioralSum < 4){ document.querySelector("#b1").src = "/images/houseParts/roof-1.svg" }
-else if(behavioralSum == 4 || behavioralSum == 5){ document.querySelector("#b2").src = "/images/houseParts/roof-2.svg" }
-else if(behavioralSum == 6){ document.querySelector("#b3").src = "/images/houseParts/roof-3.svg" }
-else if(behavioralSum == 7 || behavioralSum == 8){ document.querySelector("#b4").src = "/images/houseParts/roof-4.svg" }
-else if(behavioralSum == 9 || behavioralSum == 10){ document.querySelector("#b5").src = "/images/houseParts/roof-5.svg" }
-if(physicalSum < 4){ document.querySelector("#p1").src = "/images/houseParts/base-1.svg" }
-else if(physicalSum == 4 || physicalSum == 5){ document.querySelector("#b2").src = "/images/houseParts/base-2.svg" }
-else if(physicalSum == 6){ document.querySelector("#b3").src = "/images/houseParts/base-3.svg" }
-else if(physicalSum == 7 || physicalSum == 8){ document.querySelector("#b4").src = "/images/houseParts/base-4.svg" }
-else if(physicalSum == 9 || physicalSum == 10){ document.querySelector("#b5").src = "/images/houseParts/base-5.svg" }
+if(behavioralSum < 4){ document.querySelector("#b1").src = "images/houseParts/roof-1.svg" }
+else if(behavioralSum == 4 || behavioralSum == 5){ document.querySelector("#b2").src = "images/houseParts/roof-2.svg" }
+else if(behavioralSum == 6){ document.querySelector("#b3").src = "images/houseParts/roof-3.svg" }
+else if(behavioralSum == 7 || behavioralSum == 8){ document.querySelector("#b4").src = "images/houseParts/roof-4.svg" }
+else if(behavioralSum == 9 || behavioralSum == 10){ document.querySelector("#b5").src = "images/houseParts/roof-5.svg" }
+if(physicalSum < 4){ document.querySelector("#p1").src = "images/houseParts/base-1.svg" }
+else if(physicalSum == 4 || physicalSum == 5){ document.querySelector("#b2").src = "images/houseParts/base-2.svg" }
+else if(physicalSum == 6){ document.querySelector("#b3").src = "images/houseParts/base-3.svg" }
+else if(physicalSum == 7 || physicalSum == 8){ document.querySelector("#b4").src = "images/houseParts/base-4.svg" }
+else if(physicalSum == 9 || physicalSum == 10){ document.querySelector("#b5").src = "images/houseParts/base-5.svg" }
 
 if(socialSum < 4){ 
-    document.querySelector("#s1").src = "/images/houseParts/SOC-1.svg" 
+    document.querySelector("#s1").src = "images/houseParts/SOC-1.svg" 
 }
 else if(socialSum == 4 || socialSum == 5){ 
-    if(CESum < 4){document.querySelector("#s2").src = "/images/houseParts/CE-1.svg" }
-    else if(CESum == 4 || CESum == 5){document.querySelector("#s2").src = "/images/houseParts/CE-2.svg" }
-    else if(CESum == 6){document.querySelector("#s2").src = "/images/houseParts/CE-3.svg" }
-    else if(CESum == 7 || CESum == 8){document.querySelector("#s2").src = "/images/houseParts/CE-4.svg" }
-    else if(CESum == 9 || CESum == 10){document.querySelector("#s2").src = "/images/houseParts/CE-5.svg" }
+    if(CESum < 4){document.querySelector("#s2").src = "images/houseParts/CE-1.svg" }
+    else if(CESum == 4 || CESum == 5){document.querySelector("#s2").src = "images/houseParts/CE-2.svg" }
+    else if(CESum == 6){document.querySelector("#s2").src = "images/houseParts/CE-3.svg" }
+    else if(CESum == 7 || CESum == 8){document.querySelector("#s2").src = "images/houseParts/CE-4.svg" }
+    else if(CESum == 9 || CESum == 10){document.querySelector("#s2").src = "images/houseParts/CE-5.svg" }
 }
 else if(socialSum == 6){ 
-    if(CESum < 4){document.querySelector("#s3").src = "/images/houseParts/SOC-3-CE-1.svg" }
-    else if(CESum == 4 || CESum == 5){document.querySelector("#s3").src = "/images/houseParts/SOC-3-CE-2.svg" }
-    else if(CESum == 6){document.querySelector("#s3").src = "/images/houseParts/SOC-3-CE-3.svg" }
-    else if(CESum == 7 || CESum == 8){document.querySelector("#s3").src = "/images/houseParts/SOC-3-CE-4.svg" }
-    else if(CESum == 9 || CESum == 10){document.querySelector("#s3").src = "/images/houseParts/SOC-3-CE-5.svg" }
+    if(CESum < 4){document.querySelector("#s3").src = "images/houseParts/SOC-3-CE-1.svg" }
+    else if(CESum == 4 || CESum == 5){document.querySelector("#s3").src = "images/houseParts/SOC-3-CE-2.svg" }
+    else if(CESum == 6){document.querySelector("#s3").src = "images/houseParts/SOC-3-CE-3.svg" }
+    else if(CESum == 7 || CESum == 8){document.querySelector("#s3").src = "images/houseParts/SOC-3-CE-4.svg" }
+    else if(CESum == 9 || CESum == 10){document.querySelector("#s3").src = "images/houseParts/SOC-3-CE-5.svg" }
 }
 else if(socialSum == 7 || socialSum == 8){ 
-    if(CESum < 4){document.querySelector("#s4").src = "/images/houseParts/SOC-4-CE-1.svg" }
-    else if(CESum == 4 || CESum == 5){document.querySelector("#s4").src = "/images/houseParts/SOC-4-CE-2.svg" }
-    else if(CESum == 6){document.querySelector("#s4").src = "/images/houseParts/SOC-4-CE-3.svg" }
-    else if(CESum == 7 || CESum == 8){document.querySelector("#s4").src = "/images/houseParts/SOC-4-CE-4.svg" }
-    else if(CESum == 9 || CESum == 10){document.querySelector("#s4").src = "/images/houseParts/SOC-4-CE-5.svg" }
+    if(CESum < 4){document.querySelector("#s4").src = "images/houseParts/SOC-4-CE-1.svg" }
+    else if(CESum == 4 || CESum == 5){document.querySelector("#s4").src = "images/houseParts/SOC-4-CE-2.svg" }
+    else if(CESum == 6){document.querySelector("#s4").src = "images/houseParts/SOC-4-CE-3.svg" }
+    else if(CESum == 7 || CESum == 8){document.querySelector("#s4").src = "images/houseParts/SOC-4-CE-4.svg" }
+    else if(CESum == 9 || CESum == 10){document.querySelector("#s4").src = "images/houseParts/SOC-4-CE-5.svg" }
 }
 else if(socialSum == 9 || socialSum == 10){ 
-    if(CESum < 4){document.querySelector("#s5").src = "/images/houseParts/SOC-5-CE-1.svg" }
-    else if(CESum == 4 || CESum == 5){document.querySelector("#s5").src = "/images/houseParts/SOC-5-CE-2.svg" }
-    else if(CESum == 6){document.querySelector("#s5").src = "/images/houseParts/SOC-5-CE-3.svg" }
-    else if(CESum == 7 || CESum == 8){document.querySelector("#s5").src = "/images/houseParts/SOC-5-CE-4.svg" }
-    else if(CESum == 9 || CESum == 10){document.querySelector("#s5").src = "/images/houseParts/SOC-5-CE-5.svg" }
+    if(CESum < 4){document.querySelector("#s5").src = "images/houseParts/SOC-5-CE-1.svg" }
+    else if(CESum == 4 || CESum == 5){document.querySelector("#s5").src = "images/houseParts/SOC-5-CE-2.svg" }
+    else if(CESum == 6){document.querySelector("#s5").src = "images/houseParts/SOC-5-CE-3.svg" }
+    else if(CESum == 7 || CESum == 8){document.querySelector("#s5").src = "images/houseParts/SOC-5-CE-4.svg" }
+    else if(CESum == 9 || CESum == 10){document.querySelector("#s5").src = "images/houseParts/SOC-5-CE-5.svg" }
 }
 
-if(CESum < 4){document.querySelector("#ce1").src = "/images/houseParts/CE-1.svg" }
-else if(CESum == 4 || CESum == 5){document.querySelector("#ce2").src = "/images/houseParts/CE-2.svg" }
-else if(CESum == 6){document.querySelector("#ce3").src = "/images/houseParts/CE-3.svg" }
-else if(CESum == 7 || CESum == 8){document.querySelector("#ce4").src = "/images/houseParts/CE-4.svg" }
-else if(CESum == 9 || CESum == 10){document.querySelector("#ce5").src = "/images/houseParts/CE-5.svg" }
+if(CESum < 4){document.querySelector("#ce1").src = "images/houseParts/CE-1.svg" }
+else if(CESum == 4 || CESum == 5){document.querySelector("#ce2").src = "images/houseParts/CE-2.svg" }
+else if(CESum == 6){document.querySelector("#ce3").src = "images/houseParts/CE-3.svg" }
+else if(CESum == 7 || CESum == 8){document.querySelector("#ce4").src = "images/houseParts/CE-4.svg" }
+else if(CESum == 9 || CESum == 10){document.querySelector("#ce5").src = "images/houseParts/CE-5.svg" }
 
 
 // scroll to top ------------------------------------------------------------------------------------------------------
