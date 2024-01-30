826087a9e871e4d97ba0da8853850b451d3bf7fe
Janice Ji <jij021@newschool.edu>
2024-01-30 00:30:34 -0500

    added timer and animations

--- spring-2024/checkmark-race/scripts/script.js
+++ spring-2024/checkmark-race/scripts/script.js
@@ -1,26 +1,94 @@
-// give the inputs each a unique id
+// give the inputs each a unique id -------------------------------
 
 let liArray = document.querySelectorAll("li");
 let inputArray = document.querySelectorAll("input");
-let count = 0;
+
 for(i = 0; i < liArray.length; i++){
     liArray[i].id = "li-" + i;
     inputArray[i].id = i;
 }
 
-// click the start button, hide it, and start the timer <<<<<<<<<<<<<<<<<<<<<<<<<<
+// disable everything after the 1st one, and then keep going -------------------------------
+
+let count = 0;
+function enable() {
+    count++;
+    document.getElementById(count).disabled = false;
+    document.getElementById("li-" + count).style.color = "black";
+}
+
+// click the start button, hide it, and start the timer -------------------------------
+
+let minutes = 0;
+let seconds = 0;
+let mil = 0;
+
+function format() {
+  let str = "";
+  if (minutes < 10) {
+    str += "0" + minutes;
+  } else {
+    str += minutes;
+  }
+  str += ":";
+  if (seconds < 10) {
+    str += "0" + seconds;
+  } else {
+    str += seconds;
+  }
+  str += ":";
+  if (mil < 100) {
+    str += "0" + mil;
+  } else {
+    str += mil;
+  }
+  return str.substring(0, str.length - 1);
+}
+
+function check() {
+  if (mil === 1000) {
+    mil = 0;
+    seconds += 1;
+  }
+  if (seconds === 60) {
+    seconds = 0;
+    minutes += 1;
+  }
+  if (minutes === 60) {
+    minutes = 0;
+  }
+}
+
+let interval = null;
+
 function start() {
     document.querySelector("#start-button").style.display = "none";
     document.querySelector("#checkboxes").style.display = "block";
     document.querySelector("#timer-container").style.color = "black";
+    interval = setInterval(
+        function timer() {
+            mil += 10;
+            check();
+            document.querySelector('#timer').innerHTML = "Timer: " + format();
+        }
+    , 10);
 }
 
-// move the inputs left or right randomly if you checked the box right before it
+function stop() {
+    if(document.querySelectorAll('input[type="checkbox"]:checked').length === 45){
+        clearInterval(interval);
+        // confetti from confetti.js.org
+        confetti({
+            particleCount: 250,
+            spread: 160,
+            origin: { y: 0.9 },
+        });
+    }
+}
 
-// function that returns the id of a checked checkbox (via onclick)
-// store the most recently clicked checkbox in this variable
+// move the inputs left or right randomly if you checked the box right before it -------------------------------
+
 let recentClick;
-// the id of the checkbox after the most recent one
 let nextNum; 
 function returnId(idNum) {
     recentClick = idNum;
@@ -38,7 +106,6 @@ function chooseLeftOrRight() {
 }
 
 // move the checkboxes
-// track how much to move the id down
 let translateY = 0;
 function moveInput() {
     // check if this id is checked
@@ -55,8 +122,13 @@ function moveInput() {
             document.getElementById("li-" + nextNum).style.marginLeft = "-" + getRandomInt(0,3) + "em";
             document.getElementById("checkboxes").style.transform = "translateY(" + translateY + "px)";
         }
-        
     } else {
         // dont do anything
     }
 }
+
+// scoreboard sorting
+// convert the timer to a string
+// add everyones score to an array
+// sort the array with .sort
+// create a list from the sorted array
